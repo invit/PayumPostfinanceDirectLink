617061b4bce6fd2e5c705f59dc7aa0c8c5286b39
Toni Rudolf <toni.rudolf@gmail.com>
2016-11-09 15:51:47 +0100

    added amount to capture

--- Api.php
+++ Api.php
@@ -137,6 +137,7 @@ class Api
             $operation = DirectLinkMaintenanceRequest::OPERATION_AUTHORISATION_DELETE_AND_CLOSE;
         }
 
+        $directLinkRequest->setAmount($fields['amount']);
         $directLinkRequest->setOperation($operation);
         $directLinkRequest->validate();
 
